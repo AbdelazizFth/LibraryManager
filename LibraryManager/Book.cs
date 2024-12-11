f103a2eb8e97de64d713d2d931f92e4fda734db4
Abdelaziz FATHALLAH <azizfathallah50@gmail.com>
2024-12-11 16:36:07 +0100

    Création des classes

---
+++ LibraryManager/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager
+{
+    public class Book
+    {
+        public string title { get; set; }
+
+        public string author { get; set; }
+
+        public int publicationYear { get; set; } 
+
+        public bool isAvailable { get; set; } = true;
+
+    }
+}
